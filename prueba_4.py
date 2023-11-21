007a6b85e604f966ea0c2ba7c2a4f1a9a9834f52
Gian Quezada <150297069+gcdavidq@users.noreply.github.com>
2023-11-21 01:49:24 -0500

    Add files via upload

--- prueba_4.py
+++ prueba_4.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import streamlit as st
-from PIL import Image
 st.set_page_config(page_title="Encuesta Oficial EPS") # Nombre para configurar la pagina web
 st.header('Resultados Encuestas Nacionales EPS Colombia 2022') #Va a ser el titulo de la pagina
 st.subheader('Cómo perciben los ciudadanos el servicio de las EPS en Colombia?') #Subtitulo
