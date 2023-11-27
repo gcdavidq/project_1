82096e0b5731dcf0dbeaa6587b50b292a6752054
Magno-Luque <140691274+Magno-Luque@users.noreply.github.com>
2023-11-27 16:14:17 -0500

    Update prueba_4.py

--- prueba_4.py
+++ prueba_4.py
@@ -263,93 +263,200 @@ st.divider()
 
 
 
-excel_file = 'prueba_project.xlsx'  # Nombre del archivo a importar
-sheet_name = 'Hoja1'  # Nombre de la hoja de Excel que voy a importar
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+import plotly.express as px
+import folium
+from streamlit_folium import folium_static
 
-df = pd.read_excel(excel_file, sheet_name=sheet_name, usecols='A:H', header=1)
+# Configuración de la página
+st.set_page_config(
+    page_title="Sismos en el Perú",
+    page_icon="volcano",
+    initial_sidebar_state="expanded",
+)
+
+# Título principal
+st.title('Análisis de Sismos en el Perú (1960-2022)')
+
+
+df = pd.read_excel("prueba_project_5000.xlsx")
 
 # Cambiar los nombres de las columnas
-df = df.rename(columns={7.5: 'Magnitud', 19600113: 'Fecha_UTC', -16.145: 'Latitud', -72.144: 'Longitud'})
 
 # Extraer Año, Mes y Día de la columna Fecha_UTC
-df['Año'] = df['Fecha_UTC'].astype(str).str[:4]
-df['Mes'] = df['Fecha_UTC'].astype(str).str[4:6]
-df['Día'] = df['Fecha_UTC'].astype(str).str[6:]
+df['Año'] = df['FECHA_UTC'].astype(str).str[:4]
+df['Mes'] = df['FECHA_UTC'].astype(str).str[4:6]
+df['Día'] = df['FECHA_UTC'].astype(str).str[6:]
 
-conteo_magnitud = df['Magnitud'].value_counts()
-df_conteo = pd.DataFrame({'MAGNITUD': conteo_magnitud.index, 'FRECUENCIA': conteo_magnitud.values})
+st.subheader('PROFUNDIDAD DE LOS SISMOS TOMANDO EN CUENTA LOS AÑOS')  # Subtítulo
+# Crear slider para seleccionar un rango de años de 'Fecha_UTC'
+min_year = df['Año'].astype(int).min()
+max_year = df['Año'].astype(int).max()
 
-# Rangos
-rangos = pd.cut(df['Magnitud'], bins=5)
-df['Rango'] = rangos.astype(str)
+st.text("SELECCIONE EL RANGO DE AÑOS EN EL QUE DESEA VER LAS TABLAS Y EL GRAFICO DE PROFUNDIDAD")
+selected_years = st.slider('Seleccione:',
+                          min_value=min_year,
+                          max_value=max_year,
+                          value=(min_year, max_year),
+                          key="select_slider_years")
 
-# DataFrame de frecuencia por rango
-conteo_rangos = rangos.value_counts().sort_index()
-df_conteo_rangos = pd.DataFrame({'RANGO': [str(rango) for rango in conteo_rangos.index], 'FRECUENCIA': conteo_rangos.values})
 
-st.dataframe(df)  # Mostrar el DataFrame en Streamlit
-st.dataframe(df_conteo)
-st.write(df_conteo)
-st.dataframe(df_conteo_rangos)
-st.write(df_conteo)
 
-# Crear gráfico Plotly Express
-fig = px.bar(df_conteo_rangos, x='RANGO', y='FRECUENCIA', color='RANGO', labels={'FRECUENCIA': 'Frecuencia'})
-fig.update_layout(title='Frecuencia de Sismos en Rangos', xaxis_title='Rango', yaxis_title='Frecuencia')
+# Crear un DataFrame con los valores de Profundidad y su frecuencia
+conteo_profundidad = df['PROFUNDIDAD'].value_counts()
+df_profundidad = pd.DataFrame({'Profundidad': conteo_profundidad.index, 'Frecuencia': conteo_profundidad.values})
+
+# Rangos para Profundidad
+rangos_profundidad = pd.cut(df['PROFUNDIDAD'], bins=5)
+df['Rango_Profundidad'] = rangos_profundidad.astype(str)
+
+# Actualizar el DataFrame de frecuencia por rango de Profundidad
+conteo_rangos_profundidad = pd.cut(df['PROFUNDIDAD'], bins=5).value_counts().sort_index()
+df_conteo_rangos_profundidad = pd.DataFrame({'RANGO_PROFUNDIDAD': [str(rango) for rango in conteo_rangos_profundidad.index],
+                                            'FRECUENCIA_PROFUNDIDAD': conteo_rangos_profundidad.values})
+
+# Filtrar el DataFrame por el rango seleccionado de años
+df_filtrado = df[(df['Año'].astype(int) >= selected_years[0]) & (df['Año'].astype(int) <= selected_years[1])]
+
+# Actualizar el DataFrame de Profundidad y Frecuencia de acuerdo al año seleccionado
+conteo_profundidad_filtrado = df_filtrado['PROFUNDIDAD'].value_counts()
+df_profundidad_filtrado = pd.DataFrame({'PROFUNDIDAD': conteo_profundidad_filtrado.index, 'Frecuencia': conteo_profundidad_filtrado.values})
+
+# Actualizar el DataFrame de frecuencia por rango de Profundidad de acuerdo al año seleccionado
+conteo_rangos_profundidad_filtrado = pd.cut(df_filtrado['PROFUNDIDAD'], bins=5).value_counts().sort_index()
+df_conteo_rangos_profundidad_filtrado = pd.DataFrame({'RANGO_PROFUNDIDAD': [str(rango) for rango in conteo_rangos_profundidad_filtrado.index],
+                                                      'FRECUENCIA_PROFUNDIDAD': conteo_rangos_profundidad_filtrado.values})
+
+# Mostrar los DataFrames en Streamlit
+st.text("TABLA DE FRECUENCIA PARA UN RANGO DE PROFUNDIDAD")
+st.dataframe(df_conteo_rangos_profundidad_filtrado) #MOSTRAR EL DATAFRAME DE RANGOS DE PROFUNIDAD RESPECTO A LOS AÑOS SELECCIONADOS
+st.text("TABLA DE FRECUENCIA PARA PROFUNIDADES PUNTUALES")
+st.dataframe(df_profundidad_filtrado) #MOSTRAR EL DATAFRAME DEL CONTEO DE PROFUNIDAD RESPECTO A LOS AÑOS SELECCIONADOS
+
+# Crear gráfico Plotly Express con el Dataframe de Rangos actualizado
+fig = px.bar(df_conteo_rangos_profundidad_filtrado, x='RANGO_PROFUNDIDAD', y='FRECUENCIA_PROFUNDIDAD', color='RANGO_PROFUNDIDAD', labels={'FRECUENCIA_PROFUNDIDAD': 'Frecuencia'})
+fig.update_layout(title=f'Frecuencia de Sismos en Rangos de Profundidad ({selected_years[0]} - {selected_years[1]})', xaxis_title='Rango de Profundidad', yaxis_title='Frecuencia')
+
 
 # Mostrar el gráfico en Streamlit
+st.text("GRAFICO DE BARRAS DE LOS RANGOS DE PROFUNIDAD PRESENTES EN LOS SISMOS RESPECTO A LOS AÑOS SELECCIONADOS")
 st.plotly_chart(fig)
+
+
+#----------------------------------------------------------------------------
+st.subheader(' MAGNITUD DE LOS SISMOS TOMANDO EN CUENTA LOS AÑOS')
+st.text("SELECCIONE EL RANGO DE AÑOS EN EL QUE DESEA VER LAS TABLAS Y EL GRAFICO DE MAGNITUD")
+selected_year = st.slider('Seleccione:',
+                          min_value=min_year,
+                          max_value=max_year,
+                          value=(min_year, max_year),
+                          key="select_slider_year")
+
+
+
+# Crear un DataFrame con los valores de Magnitud y su frecuencia
+conteo_magnitud = df['MAGNITUD'].value_counts()
+df_magnitud = pd.DataFrame({'MAGNITUD': conteo_magnitud.index, 'Frecuencia': conteo_magnitud.values})
+
+# Rangos para Magnitud
+rangos_magnitud= pd.cut(df['MAGNITUD'], bins=5)
+df['Rango_Magnitud'] = rangos_magnitud.astype(str)
+
+# Actualizar el DataFrame de frecuencia por rango de Profundidad
+conteo_rangos_magnitud = pd.cut(df['MAGNITUD'], bins=5).value_counts().sort_index()
+df_conteo_rangos_magnitud = pd.DataFrame({'RANGO_MAGNITUD': [str(rango) for rango in conteo_rangos_magnitud.index],
+                                            'FRECUENCIA_MAGNITUD': conteo_rangos_magnitud.values})
+
+# Filtrar el DataFrame por el rango seleccionado de años
+df_filtrado_ = df[(df['Año'].astype(int) >= selected_year[0]) & (df['Año'].astype(int) <= selected_year[1])]
+
+# Actualizar el DataFrame de Profundidad y Frecuencia de acuerdo al año seleccionado
+conteo_magnitud_filtrado = df_filtrado_['MAGNITUD'].value_counts()
+df_magnitud_filtrado = pd.DataFrame({'MAGNITUD': conteo_magnitud_filtrado.index, 'Frecuencia': conteo_magnitud_filtrado.values})
+
+# Actualizar el DataFrame de frecuencia por rango de Profundidad de acuerdo al año seleccionado
+conteo_rangos_magnitud_filtrado = pd.cut(df_filtrado_['MAGNITUD'], bins=5).value_counts().sort_index()
+df_conteo_rangos_magnitud_filtrado = pd.DataFrame({'RANGO_MAGNITUD': [str(rango) for rango in conteo_rangos_magnitud_filtrado.index],
+                                                      'FRECUENCIA_MAGNITUD': conteo_rangos_magnitud_filtrado.values})
+
+# Mostrar los DataFrames en Streamlit
+st.text("TABLA DE FRECUENCIA PARA UN RANGO DE MAGNITUD")
+st.dataframe(df_conteo_rangos_magnitud_filtrado) #MOSTRAR EL DATAFRAME DE RANGOS DE PROFUNIDAD RESPECTO A LOS AÑOS SELECCIONADOS
+st.text("TABLA DE FRECUENCIA PARA PROFUNIDADES MAGNITUD")
+st.dataframe(df_magnitud_filtrado) #MOSTRAR EL DATAFRAME DEL CONTEO DE PROFUNIDAD RESPECTO A LOS AÑOS SELECCIONADOS
+
+# Crear gráfico Plotly Express con el Dataframe de Rangos actualizado
+fig_ = px.bar(df_conteo_rangos_magnitud_filtrado, x='RANGO_MAGNITUD', y='FRECUENCIA_MAGNITUD', color='RANGO_MAGNITUD', labels={'FRECUENCIA_MAGNITUD': 'Frecuencia'})
+fig_.update_layout(title=f'Frecuencia de Sismos en Rangos de Magnitud ({selected_year[0]} - {selected_year[1]})', xaxis_title='Rango de Magnitud', yaxis_title='Frecuencia')
+
+
+# Mostrar el gráfico en Streamlit
+st.text("GRAFICO DE BARRAS DE LOS RANGOS DE MAGNITUD PRESENTES EN LOS SISMOS RESPECTO A LOS AÑOS SELECCIONADOS")
+st.plotly_chart(fig_)
+
+#_________________________________________________________________________________________________
 
 # USAR LAS CORDENADAS
 st.subheader('MAPA ESTATICO')
 
 # Configurar el mapa centrado en la primera ubicación
-mapa = fl.Map(location=[df['Latitud'].iloc[0], df['Longitud'].iloc[0]], zoom_start=10)
+mapa = folium.Map(location=[df['LATITUD'].iloc[0], df['LONGITUD'].iloc[0]], zoom_start=10)
 
 # Añadir marcadores al mapa para cada ubicación en el DataFrame
 for i, row in df.iterrows():
-    fl.Marker([row['Latitud'], row['Longitud']], popup=f"Valor: {row['Magnitud']}").add_to(mapa)
+    folium.Marker([row['LATITUD'], row['LONGITUD']], popup=f"Magnitud: {row['MAGNITUD']}").add_to(mapa)
 
 # Mostrar el mapa en Streamlit
 folium_static(mapa)
+#------------------------------------------
 
-st.subheader('GRAFICO CON SLIDER')
+# Crear el mapa con el sismo de mayor magnitud por año
+st.subheader('MAPA CON LA MAYOR MAGNITUD DE CADA AÑO')
 
-# GRAFICO CON SLIDER
-# Crear rangos con la misma longitud para la columna 'Magnitud'
-rangos = pd.cut(df['Magnitud'], bins=5)
-# Convertir los Intervalos a cadenas
-df['Rango'] = rangos.astype(str)
+# Mapa con el sismo de mayor magnitud por año
+df_max_magnitudes = df.loc[df.groupby('Año')['MAGNITUD'].idxmax()]
 
-# Crear slider para seleccionar un rango de años de 'Fecha_UTC'
-min_year = df['Año'].astype(int).min()
-max_year = df['Año'].astype(int).max()
+# Configurar el mapa centrado en la primera ubicación
+mapa_max_magnitudes = folium.Map(location=[df_max_magnitudes['LATITUD'].iloc[0], df_max_magnitudes['LONGITUD'].iloc[0]], zoom_start=6)
 
-selected_years = st.select_slider('Selecciona un rango de años de Fecha_UTC',
-                                  options=list(range(min_year, max_year + 1)),
-                                  value=(min_year, max_year),
-                                  key="select_slider_19600113")
+# Añadir marcadores al mapa para cada sismo con mayor magnitud por año
+for i, row in df_max_magnitudes.iterrows():
+    folium.Marker([row['LATITUD'], row['LONGITUD']],
+                  popup=f"Año: {row['Año']}, Magnitud: {row['MAGNITUD']}",
+                  tooltip=f"Sismo en {row['Año']}").add_to(mapa_max_magnitudes)
 
-# Filtrar el DataFrame por el rango seleccionado de años
-df_filtrado = df[(df['Año'].astype(int) >= selected_years[0]) & (df['Año'].astype(int) <= selected_years[1])]
+# Mostrar el mapa en Streamlit
+folium_static(mapa_max_magnitudes)
 
-# Actualizar la gráfica de barras con el DataFrame filtrado
-conteo_edades_filtrado = df_filtrado['Rango'].value_counts().sort_index()
-df_conteo_filtrado = pd.DataFrame({'MAGNITUD': conteo_edades_filtrado.index, 'FRECUENCIA': conteo_edades_filtrado.values})
 
-# Crear la gráfica de barras actualizada
-fig = px.bar(df_conteo_filtrado, x='MAGNITUD', y='FRECUENCIA', color='MAGNITUD', labels={'FRECUENCIA': 'Frecuencia'})
-fig.update_layout(title=f'Frecuencia de Sismos en Rangos (Valor 19600113 entre {selected_years[0]} y {selected_years[1]})',
-                  xaxis_title='Rango', yaxis_title='Frecuencia')
+#-----------------------------------------------
+# Crear el mapa con el sismo de mayor magnitud por año
+st.subheader('MAPA CON LA MAYOR PROFUNIDAD DE CADA AÑO')
 
-# Mostrarlo en streamlit
-st.plotly_chart(fig)
+# Mapa con el sismo de mayor magnitud por año
+df_max_profundidades = df.loc[df.groupby('Año')['PROFUNDIDAD'].idxmax()]
 
-st.subheader('Mapa con Opción de Selección')
-# Mapa dinámico___
-# Crear slider para los sismos
-min_value_7_5 = df['Magnitud'].min()
-max_value_7_5 = df['Magnitud'].max()
+# Configurar el mapa centrado en la primera ubicación
+mapa_max_profundidades = folium.Map(location=[df_max_profundidades['LATITUD'].iloc[0], df_max_profundidades['LONGITUD'].iloc[0]], zoom_start=6)
+
+# Añadir marcadores al mapa para cada sismo con mayor magnitud por año
+for i, row in df_max_profundidades.iterrows():
+    folium.Marker([row['LATITUD'], row['LONGITUD']],
+                  popup=f"Año: {row['Año']}, Profundidad: {row['PROFUNDIDAD']} Km.",
+                  tooltip=f"Sismo en {row['Año']}").add_to(mapa_max_profundidades)
+
+# Mostrar el mapa en Streamlit
+folium_static(mapa_max_profundidades)
+#--------------------------------------------
+
+# Mapa con opción de selección
+st.subheader('MAPA DE MAGNITUD TOMANDO EN CUENTA LA SELECCION DE LOS AÑOS')
+
+# Crear slider para la magnitud de los sismos
+min_value_7_5 = df['MAGNITUD'].min()
+max_value_7_5 = df['MAGNITUD'].max()
 
 min_selected_value_7_5, max_selected_value_7_5 = st.slider(
     'Selecciona un rango de valores de Magnitud',
@@ -357,7 +464,6 @@ min_selected_value_7_5, max_selected_value_7_5 = st.slider(
     key="slider_7_5"
 )
 
-# MAPA CON OPCIÓN DE SELECCIÓN
 # Crear opciones de selección para año y mes mínimo
 min_year_option = st.selectbox('Selecciona el año mínimo', options=list(range(min_year, max_year + 1)))
 
@@ -379,18 +485,21 @@ max_month_option_num = meses_a_numeros[max_month_option]
 df_filtrado_opcion = df[
     (df['Año'].astype(int) >= min_year_option) & (df['Mes'].astype(int) >= int(min_month_option_num)) &
     (df['Año'].astype(int) <= max_year_option) & (df['Mes'].astype(int) <= int(max_month_option_num)) &
-    (df['Magnitud'] >= min_selected_value_7_5) & (df['Magnitud'] <= max_selected_value_7_5)
+    (df['MAGNITUD'] >= min_selected_value_7_5) & (df['MAGNITUD'] <= max_selected_value_7_5)
 ]
 
 # Actualizar el mapa con los filtros de opción de selección
 if not df_filtrado_opcion.empty:
-    mapa_filtrado_opcion = fl.Map(location=[df_filtrado_opcion['Latitud'].iloc[0], df_filtrado_opcion['Longitud'].iloc[0]],
+    mapa_filtrado_opcion = folium.Map(location=[df_filtrado_opcion['LATITUD'].iloc[0], df_filtrado_opcion['LONGITUD'].iloc[0]],
                                       zoom_start=10)
 
     for i, row in df_filtrado_opcion.iterrows():
-        fl.Marker([row['Latitud'], row['Longitud']], popup=f"Valor: {row['Magnitud']}").add_to(mapa_filtrado_opcion)
+        folium.Marker([row['LATITUD'], row['LONGITUD']], popup=f"MAGNITUD: {row['MAGNITUD']}").add_to(mapa_filtrado_opcion)
 
     # Mostrar el mapa filtrado en Streamlit
     folium_static(mapa_filtrado_opcion)
 else:
     st.warning("No hay datos disponibles para los filtros seleccionados.")
+
+st.subheader("TABLA CON TODOS LOS DATOS QUE SE HAN ANALIZADO")
+st.dataframe(df)
